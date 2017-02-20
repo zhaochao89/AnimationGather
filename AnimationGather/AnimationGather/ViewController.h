9f103ba3ed79528924b003d3604a4391ceb21abe
zhaochao <zhaochao@lingdanet.com>
2017-02-20 17:31:24 +0800

    init

---
+++ AnimationGather/AnimationGather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimationGather
+//
+//  Created by zhaochao on 2017/2/20.
+//  Copyright © 2017年 zhaochao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AnimationGather/AnimationGather/ViewController.m
@@ -0,0 +1,37 @@
+//
+//  ViewController.m
+//  AnimationGather
+//
+//  Created by zhaochao on 2017/2/20.
+//  Copyright © 2017年 zhaochao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (nonatomic, strong) UIButton *animationBtn;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.animationBtn = [[UIButton alloc] initWithFrame:CGRectMake(30, 50, 0, 0)];
+    [self.animationBtn setTitle:@"➕" forState:UIControlStateNormal];
+    self.animationBtn.titleLabel.font = [UIFont systemFontOfSize:18];
+    [self.animationBtn sizeToFit];
+    [self.view addSubview:self.animationBtn];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
